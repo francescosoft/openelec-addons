09b03152b2c988bd9d477fa9835803759458669d
Franz Osvald <fo@cylex.ro>
2014-08-15 17:41:39 +0300

    updated VDR Manager to 0.4

--- plugin.video.vdrmanager/addon.py
+++ plugin.video.vdrmanager/addon.py
@@ -5,6 +5,7 @@ plugin = Plugin()
 
 
 @plugin.route('/')
+#@cached_route()
 def index():
     item = {
         'label': 'Start VDR',
@@ -14,9 +15,20 @@ def index():
     return [item]
 
 @plugin.route('/startvdr/')
+#@cached_route()
 def startvdr():
-    plugin.notify(msg='Starting VDR ..', title='', delay=1000, image='')
-    os.system("/storage/.xbmc/addons/service.multimedia.vdr-addon/bin/switch.sh")
+
+    #plugin.notify(msg='Starting VDR ..', title='', delay=1000, image='')
+    if os.path.isfile("/storage/.config/udev.rules.d/66-libcec-daemon.rules"):
+    	plugin.notify(msg='Starting VDR ..', title='', delay=1000, image='')
+    	os.system("/storage/.xbmc/addons/service.multimedia.vdr-addon/bin/switch.sh")
+    else:
+        if os.path.isfile("/storage/.xbmc/addons/service.multimedia.vdr-addon/bin/init.sh"):
+    	   #print "Storage path is : %s" % plugin.storage_path
+    	   plugin.notify(msg='Init Plugin and restart', title='', delay=1000, image='')
+           os.system("/storage/.xbmc/addons/service.multimedia.vdr-addon/bin/init.sh")
+        else:
+            plugin.notify(msg='Please install the VDR Addon first', title='', delay=5000, image='')
     
 if __name__ == '__main__':
     plugin.run()
